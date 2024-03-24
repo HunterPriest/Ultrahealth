038cf51667ff513d2e8e3936a9abb1f7a8e579d9
Ya_VaFel <128213334+azikev@users.noreply.github.com>
2024-03-24 20:45:51 +0500

    Create Choose Map

--- Assets/Client/Scripts/UI/Choose Level Menu.cs
+++ Assets/Client/Scripts/UI/Choose Level Menu.cs
@@ -4,9 +4,11 @@ using UnityEngine.UIElements;
 public class ChooseLevelMenu : UIToolkitElement
 {
     [SerializeField] private VisualTreeAsset _chooseLevelsAsset;
+    
+    [Header ("Maps")] 
+    [SerializeField] private Texture2D _human1;
 
     private VisualElement _chooseLevel;
-    private VisualElement _Map;
 
     protected override void Initialize()
     {
@@ -17,21 +19,33 @@ public class ChooseLevelMenu : UIToolkitElement
     {
         ResetContainer(_chooseLevel);
 
-        _Map = _container.Q<VisualElement>("Map");
-        _Map.visible = false;
+        Label bolezni = _container.Q<Label>("Bolezni");
+        Label StartPoint = _container.Q<Label>("StartPoint");
+        Label FinishPoint = _container.Q<Label>("Finish");
+        VisualElement mapInChoose = _container.Q<VisualElement>("MapInChoose");
+        VisualElement cont = _container.Q<VisualElement>("cont");
+        Button Start = _container.Q<Button>("Start");
+        Button human1 = _container.Q<Button>("Organizm1");
 
-        Button Organ1 = _container.Q<Button>("Organ1");
+        cont.visible = false;
 
-        Organ1.clicked += () => OpenOrganMap();
+        human1.clicked += (() =>
+        {
+            bolezni.text = "1) Ультракил головного мозга  " +
+            "2) мама пропала " +
+            "3) Артем из подвала не выпустил " +
+            "4) Был рядом с Шешуковым и теперь страдает публиками";
+            StartPoint.text = "Ротовая полость";
+            FinishPoint.text = "Легкие";
+            mapInChoose.style.backgroundImage = _human1;
+            Start.clicked += () => _gameMachine.LoadLevel(1);
+            cont.visible = true;
+        }); // its a plan for bind button Human
     }
 
     private void OpenOrganMap()
     {
-        _Map.visible = false;
-        Button start = _container.Q<Button>("Start");
-
-        start.clicked += () => _gameMachine.LoadLevel(1);
-        _Map.visible = true;
+        
     }
 
     protected override void ResetContainer(VisualElement element)
