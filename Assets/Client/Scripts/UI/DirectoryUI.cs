f4506e3d02ebe7ac93091822a5e0d9c127cc1978
Ya_VaFel <128213334+azikev@users.noreply.github.com>
2024-06-30 19:39:36 +0500

    Direstory work

--- Assets/Client/Scripts/UI/DirectoryUI.cs
+++ Assets/Client/Scripts/UI/DirectoryUI.cs
@@ -42,11 +42,34 @@ public class DirectoryUI : UIToolkitElement
         for(int i = 0; i < _enemyDirectory.Count; i++)
         {
             VisualElement currentEnemy = _container.Q<VisualElement>(_enemyDirectory[i].name.ToString());
+            Button currentButton = _container.Q<Button>(_enemyDirectory[i].name.ToString());
             VisualElement ImageInScroll = currentEnemy.Q<VisualElement>("ImageInScroll");
             Label NameInScroll = currentEnemy.Q<Label>("NameInScroll");
 
             ImageInScroll.style.backgroundImage = _enemyDirectory[i]._enemyImage;
             NameInScroll.text = _enemyDirectory[i]._name;
+            currentButton.clicked += () => OpenButton(currentButton);
         }
+    }
+
+    private void OpenButton(Button button)
+    {
+        for (int i = 0; i < _enemyDirectory.Count; i++)
+        {
+            if (_enemyDirectory[i].name == button.name)
+            {
+                EnemyDirectorySO CurrentElement = _enemyDirectory[i];
+
+                VisualElement image = _container.Q<VisualElement>("Image");
+                Label name = _container.Q<Label>("Name");
+                Label about = _container.Q<Label>("About");
+
+                image.style.backgroundImage = CurrentElement._enemyImage;
+                name.text = CurrentElement._name;
+                about.text = CurrentElement._aboutEnemy;
+            }
+        }
+
+        
     }
 }
