5368677d83ac37e7517062518c0fd1decad6a301
Ya_VaFel <128213334+azikev@users.noreply.github.com>
2024-06-30 18:43:33 +0500

    Enemy in scroll

---
+++ Assets/Client/Scripts/SO/EnemyDirectorySO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "Ultrahealth/Enemy/EnemyDirectory")]
+public class EnemyDirectorySO : ScriptableObject
+{
+    public Texture2D _enemyImage;
+    public string _name;
+    public string _aboutEnemy;
+}
--- Assets/Client/Scripts/UI/DirectoryUI.cs
+++ Assets/Client/Scripts/UI/DirectoryUI.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,6 +7,7 @@ public class DirectoryUI : UIToolkitElement
     [SerializeField] private Menu _menu;
     [SerializeField] private Pause _pause;
     [SerializeField] private VisualTreeAsset _directoryAsset;
+    [SerializeField] private List<EnemyDirectorySO> _enemyDirectory;
 
     private VisualElement _directory;
 
@@ -17,6 +19,8 @@ public class DirectoryUI : UIToolkitElement
     public void OpenDirectory()
     {
         ResetContainer(_directory);
+
+        InitializeDirectoryScroll();
 
         Button back = _container.Q<Button>("Back");
 
@@ -31,5 +35,18 @@ public class DirectoryUI : UIToolkitElement
                 _pause.OpenPause();
             }
         };
+    }
+
+    private void InitializeDirectoryScroll()
+    {
+        for(int i = 0; i < _enemyDirectory.Count; i++)
+        {
+            VisualElement currentEnemy = _container.Q<VisualElement>(_enemyDirectory[i].name.ToString());
+            VisualElement ImageInScroll = currentEnemy.Q<VisualElement>("ImageInScroll");
+            Label NameInScroll = currentEnemy.Q<Label>("NameInScroll");
+
+            ImageInScroll.style.backgroundImage = _enemyDirectory[i]._enemyImage;
+            NameInScroll.text = _enemyDirectory[i]._name;
+        }
     }
 }
