15aac53b790b62506be92ede1390d31063475cfd
Ya_VaFel <128213334+azikev@users.noreply.github.com>
2024-08-29 12:22:39 +0500

    first fix

--- Assets/Client/Scripts/UI/PlayerMenu.cs
+++ Assets/Client/Scripts/UI/PlayerMenu.cs
@@ -4,8 +4,27 @@ using UnityEngine.UIElements;
 
 public class PlayerMenu : UIToolkitElementWithExitOnButton
 {
-    [SerializeField] private VisualTreeAsset _playerMenuAsset;
     [SerializeField] private LevelChoice _levelChoice;
     [SerializeField] private levelUp _levelUp;
     [SerializeField] private DictionaryUI _directoryUI;
+    [SerializeField] private Menu _menu;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        Button levels = UIElement.Q<Button>("Levels");
+        Button levelUp = UIElement.Q<Button>("LevelUp");
+        Button directory = UIElement.Q<Button>("Directory");
+        Button exitToMenu = UIElement.Q<Button>("ExitToMenu");
+
+        levels.clicked += _levelChoice.Open;
+        levelUp.clicked += _levelUp.Open;
+        directory.clicked += _directoryUI.Open;
+        exitToMenu.clicked += _menu.Open;
+    }
+    public override void Open()
+    {
+        base.Open();
+    }
 }
